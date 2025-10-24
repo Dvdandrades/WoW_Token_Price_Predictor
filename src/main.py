74d20b0ac9c903b2e82e1079a3946044b8989073
Dvdandrades <dvdandrades@gmail.com>
2025-10-24 11:51:54 +0200

    add multi-threading

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from api_client import get_access_token, fetch_wow_token_price
-from data_manager import save_price, load_data
-from plotter import plot_history
+from data_manager import save_price
 from dashboard import app
+import time
 import schedule
+import threading
 
 def main():
     """Fetches, saves, and plots the latest WoW Token price."""
@@ -17,26 +18,20 @@ def main():
         price = data["price"]
         save_price(price)
         
-        print("Loading historical data...")
-        # Load data is here primarily to ensure the plotting function works
-        df = load_data()
-
-        print("Plotting price history...")
-        plot_history(df) # This creates the static plot.png
-        
     except Exception as e:
         print(f"An error occurred during tracking: {e}")
 
-# Call once immediately to start
-main()
-
-# Schedule the tracking function to run every 20 minutes
-schedule.every(20).minutes.do(main)
-
-print("\nScheduler started. Tracking every 20 minutes.")
-print("Starting Dash web server in debug mode...")
+def run_schedule():
+    schedule.every(20).minutes.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 # The `main` function runs every 20 minutes
 # The `app.run` command runs the web server indefinitely
 if __name__ == "__main__":
+    main()
+    scheduler_thread = threading.Thread(target=run_schedule, daemon=True)
+    scheduler_thread.start()
+    
     app.run(debug=True)
